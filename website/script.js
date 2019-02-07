e4e9abdfd8765b679a0045a5c0d890b9736cdee8
selenacl <selenacl22@gmail.com>
2019-02-07 17:57:04 -0500

    Add files via upload
    login functional, working on add contact

--- website/script.js
+++ website/script.js
@@ -1,7 +1,10 @@
 var urlBase = 'https://managebandageapi.azurewebsites.net';
 
+var username = "a";
+
 function doLogin()
 {
+
     var login = document.getElementById("inputUsername").value; //  .value gets input from "email" textbox
     var password = document.getElementById("inputPassword").value; //  .value gets input from password textbox
 
@@ -18,14 +21,17 @@ function doLogin()
     {
         xhr.send(jsonPayload); // sends to server, used for POST requests
 
-         if( xhr.responseText !== "true")
+        var jsonObject = JSON.parse(xhr.responseText);
+
+         if(jsonObject == null)
          {
              document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
              alert("login failed");
              return;
-         } else if(xhr.responseText == "true") {
-            alert("login successful");
-            window.location.href = "https://managebandage.azurewebsites.net/contacts.html"
+         } else {
+            window.username = login;
+            alert(window.username);
+            window.location.href = "file:///C:/Users/Selena/Desktop/COP-4331-Group-8/website/contacts.html"
         }
 
     //     document.getElementById("inputEmail").value = ""; // resets text boxes
@@ -40,3 +46,33 @@ function doLogin()
 
 }
 
+function addContact()
+{
+  //  alert("clicked");
+  var newFirstName = document.getElementById("First Name").value;
+  var newLastName = document.getElementById("Last Name").value;
+  var newPhoneNumber = document.getElementById("Phone Number").value;
+  var newEmail = document.getElementById("Email Address").value;
+  alert(window.username);
+
+//  var jsonPayload = '{ "UserID" : "' + username + '", "FirstName" : "' + newFirstName + '", "LastName" : "' + newLastName + '", "PhoneNumber" : "' + newPhoneNumber + '", "Email" : "' + newEmail + '" }';
+//  alert(jsonPayload);
+
+  // var url = urlBase + '/SaveContact';
+  // xhr.open("POST", url, false);
+  // xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  // try
+  // {
+  //     xhr.onreadystatechange = function()
+  //     {
+  //       if(this.readyState == 4 && this.status == 200)
+  //       {
+  //         alert("Contact added");
+  //       }
+  //     };
+  //     xhr.send(jsonPayload);
+  // } catch (err) {
+  //   alert(err);
+  // }
+
+}
