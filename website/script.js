c8354ea267cbbd358e0a706f5333be921cf3be8c
Alejandro Cortes <acortes@Alejandros-MacBook-Pro.local>
2019-02-09 09:10:13 -0500

    Update from Selena

--- website/script.js
+++ website/script.js
@@ -1,12 +1,15 @@
 var urlBase = 'https://managebandageapi.azurewebsites.net';
 
+var username = "";
+
 function doLogin()
 {
+
     var login = document.getElementById("inputUsername").value; //  .value gets input from "email" textbox
     var password = document.getElementById("inputPassword").value; //  .value gets input from password textbox
 
     document.getElementById("loginResult").innerHTML = "";
-// 
+
     var jsonPayload = '{"UserID" : "' + login + '", "Password" : "' + password + '"}';
     var url = urlBase + '/UserLogin';
 
@@ -16,7 +19,9 @@ function doLogin()
 
     try
     {
+
         xhr.send(jsonPayload); // sends to server, used for POST requests
+
         var jsonObject = JSON.parse(xhr.responseText);
 
          if(jsonObject == null)
@@ -24,9 +29,10 @@ function doLogin()
              document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
              alert("login failed");
              return;
-         } else { //if(xhr.responseText == "true") {
-            alert("login successful");
-            window.location.href = "https://managebandage.azurewebsites.net/contacts.html"
+         } else {
+            window.name = login;
+          //  alert(window.name);
+            window.location.href = "file:///C:/Users/Selena/Desktop/COP-4331-Group-8/website/contacts.html";
         }
 
     //     document.getElementById("inputEmail").value = ""; // resets text boxes
@@ -41,37 +47,151 @@ function doLogin()
 
 }
 
-
-function switchLogin()
+function addContact()
 {
-    // document.getElementById("loginBtn").style.visibility = "collapse"; 
-    removeElement("loginBtn");
-    // document.getElementById("createAcc").innerHTML = "Create account"
-    removeElement("firstTime");
-    document.getElementById("formHeader").innerHTML = "Create account";
-    
-    addElement("loginContainer", "button", "createAcc", "Create your account!");
-    document.getElementById("createAcc").className = "btn btn-lg btn-primary btn-block";
-    
-    document.getElementById("createAcc").addEventListener("click", function(){ createAccount() });
+  var newFirstName = document.getElementById("First Name").value;
+  var newLastName = document.getElementById("Last Name").value;
+  var newPhoneNumber = document.getElementById("Phone Number").value;
+  var newEmail = document.getElementById("Email Address").value;
+
+  var jsonPayload = '{ "UserID" : "' + window.name + '", "FirstName" : "' + newFirstName + '", "LastName" : "' + newLastName + '", "PhoneNumber" : "' + newPhoneNumber + '", "Email" : "' + newEmail + '" }';
+
+   var url = urlBase + '/SaveContact';
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", url, false);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try
+  {
+      xhr.onreadystatechange = function()
+      {
+        if(this.readyState == 4 && this.status == 200)
+        {
+          alert("Contact added");
+        }
+      };
+      xhr.send(jsonPayload);
+  } catch (err) {
+    alert(err);
+  }
+
 }
 
-function createAccount()
-{
-    alert("Account created");
+function createUser() {
+  var newUsername = document.getElementById("inputUsername").value;
+  var newPassword = document.getElementById("inputPassword").value;
+
+  var jsonPayload = '{"UserID" : "' + newUsername + '", "Password" : "' + newPassword + '"}';
+  var url = urlBase + '/CreateUser';
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, false);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+  try
+  {
+      xhr.onreadystatechange = function()
+      {
+        if(this.readyState == 4 && this.status == 200)
+        {
+          alert("Account created");
+          window.name = newUsername;
+          window.location.href = "file:///C:/Users/Selena/Desktop/COP-4331-Group-8/website/contacts.html";
+        }
+      };
+      xhr.send(jsonPayload);
+  } catch (err) {
+    alert(err);
+  }
+
 }
 
-function addElement(parentId, elementTag, elementId, html) {
-    // Adds an element to the document
-    var p = document.getElementById(parentId);
-    var newElement = document.createElement(elementTag);
-    newElement.setAttribute('id', elementId);
-    newElement.innerHTML = html;
-    p.appendChild(newElement);
+function getContacts() {
+  var toBeDeleted ="";
+  var jsonPayload = '{"UserID" : "' + window.name + '", "Password" : "' + "" + '"}';
+  var url = urlBase + '/GetContacts';
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, false);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+  try
+  {
+
+      xhr.send(jsonPayload); // sends to server, used for POST requests
+
+      var jsonObject = JSON.parse(xhr.responseText);
+      jsonObject = JSON.parse(jsonObject);
+
+  }
+  catch(err)
+  {
+      document.getElementById("loginResult").innerHTML = err.message;
+      alert(err);
+  }
+
+
+  var html = '';
+  for(var i in jsonObject)
+  {
+    html += '<tr id="'+i+'">';
+    html += '<td>'+jsonObject[i]["ContactID"]+'</td>';
+    html += '<td>'+jsonObject[i]["FirstName"]+'</td>';
+    html += '<td>'+jsonObject[i]["LastName"]+'</td>';
+    html += '<td>'+jsonObject[i]["PhoneNumber"]+'</td>';
+    html += '<td>'+jsonObject[i]["Email"]+'</td>';
+    html += '<td><button class="btn btn-primary" onClick="deleteContact()">Delete</button></td></tr>';
+    html += '</tr>';
+  }
+  $('#contactsData > tbody').html(html);
+
 }
-function removeElement(elementId)
-{
-    // Removes an element from the document
-    var element = document.getElementById(elementId);
-    element.parentNode.removeChild(element);
+
+function deleteContact() {
+  alert("delete clicked!");
+  var i = r.parentNode.parentNode.rowIndex;
+  document.getElementById("contactsData").deleteRow(0);
+  // delete from DB
+  // force reload / remove from table
+}
+
+function searchContacts() {
+
+  var search = document.getElementById("Search").value;
+
+  var contactList = document.getElementById("contactList");
+  contactList.innerHTML = "";
+
+
+  var jsonPayload = '{"UserID" : "' + window.name + '", "FirstName" : "' + search + '", "LastName" : "' + "" + '", "PhoneNumber" : "' + "" + '", "Email" : "' + "" + '", "ContactID" : "' + '' + '"}';
+  var url = urlBase + '/SearchContacts';
+
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, false);
+  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  try
+  {
+    xhr.onreadystatechange = function()
+    {
+      if (this.readyState == 4 && this.status == 200)
+      {
+
+        var jsonObject = JSON.parse( xhr.responseText );
+
+        jsonObject = JSON.parse(jsonObject);
+
+        var i;
+        for( i=0; i<jsonObject.length; i++ )
+        {
+          var opt = document.createElement("option");
+          opt.text = jsonObject[i]["FirstName"] + " " +jsonObject[i]["LastName"];
+          opt.value = "";
+          contactList.options.add(opt);
+        }
+      }
+    };
+    xhr.send(jsonPayload);
+  }
+  catch(err)
+  {
+    alert(err);
+  }
 }
