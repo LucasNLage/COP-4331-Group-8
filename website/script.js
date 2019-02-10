358034843cb3faa62c20bde2181a9d90076545b2
Alejandro Cortes <acortes@Alejandros-MacBook-Pro.local>
2019-02-10 01:03:58 -0500

    jQuery finds the row index when you click delete button.. but returns undefined. Needs to return integer

--- website/script.js
+++ website/script.js
@@ -32,7 +32,7 @@ function doLogin()
          } else {
             window.name = login;
           //  alert(window.name);
-            window.location.href = "file:///C:/Users/Selena/Desktop/COP-4331-Group-8/website/contacts.html";
+            window.location.href = "contacts.html";
         }
 
     //     document.getElementById("inputEmail").value = ""; // resets text boxes
@@ -49,28 +49,28 @@ function doLogin()
 
 function addContact()
 {
-  var newFirstName = document.getElementById("First Name").value;
-  var newLastName = document.getElementById("Last Name").value;
-  var newPhoneNumber = document.getElementById("Phone Number").value;
-  var newEmail = document.getElementById("Email Address").value;
-
-  var jsonPayload = '{ "UserID" : "' + window.name + '", "FirstName" : "' + newFirstName + '", "LastName" : "' + newLastName + '", "PhoneNumber" : "' + newPhoneNumber + '", "Email" : "' + newEmail + '" }';
-
-   var url = urlBase + '/SaveContact';
-   var xhr = new XMLHttpRequest();
-   xhr.open("POST", url, false);
-  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-  try
-  {
-      xhr.onreadystatechange = function()
-      {
-        if(this.readyState == 4 && this.status == 200)
+    var newFirstName = document.getElementById("First Name").value;
+    var newLastName = document.getElementById("Last Name").value;
+    var newPhoneNumber = document.getElementById("Phone Number").value;
+    var newEmail = document.getElementById("Email Address").value;
+    var jsonPayload = '{ "UserID" : "' + window.name + '", "FirstName" : "' + newFirstName + '", "LastName" : "' + newLastName + '", "PhoneNumber" : "' + newPhoneNumber + '", "Email" : "' + newEmail + '" }';
+    var url = urlBase + '/SaveContact';
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, false);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    
+    try
+    {
+        xhr.onreadystatechange = function()
         {
-          alert("Contact added");
-        }
-      };
-      xhr.send(jsonPayload);
-  } catch (err) {
+            if(this.readyState == 4 && this.status == 200)
+            {
+                alert("Contact added");
+            }
+        };
+        xhr.send(jsonPayload);
+        getContacts();
+    } catch (err) {
     alert(err);
   }
 
@@ -94,7 +94,7 @@ function createUser() {
         {
           alert("Account created");
           window.name = newUsername;
-          window.location.href = "file:///C:/Users/Selena/Desktop/COP-4331-Group-8/website/contacts.html";
+          window.location.href = "contacts.html";
         }
       };
       xhr.send(jsonPayload);
@@ -137,19 +137,111 @@ function getContacts() {
     html += '<td>'+jsonObject[i]["LastName"]+'</td>';
     html += '<td>'+jsonObject[i]["PhoneNumber"]+'</td>';
     html += '<td>'+jsonObject[i]["Email"]+'</td>';
-    html += '<td><button class="btn btn-primary" onClick="deleteContact()">Delete</button></td></tr>';
+    html += '<td><button class="btn btn-primary" onclick="getButtonIndex()">Delete</button></td></tr>';
     html += '</tr>';
   }
   $('#contactsData > tbody').html(html);
 
 }
 
+function getButtonIndex()
+{
+    var ret;    
+    $('td').click(function()
+    {
+        var row_index = $(this).parent().index();
+        ret = row_index;
+        console.log(ret);
+    
+        
+    });
+    console.log(ret);
+    return ret;
+    // return ret;
+    
+    ret = getButtonIndex();
+}
+
 function deleteContact() {
-  alert("delete clicked!");
-  var i = r.parentNode.parentNode.rowIndex;
-  document.getElementById("contactsData").deleteRow(0);
-  // delete from DB
-  // force reload / remove from table
+
+    var jsonPayload = '{"UserID" : "' + window.name + '", "Password" : "' + "" + '"}';
+    var url = urlBase + '/GetContacts';
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, false);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try
+    {
+
+        xhr.send(jsonPayload); // sends to server, used for POST requests
+        var jsonObject = JSON.parse(xhr.responseText);
+        jsonObject = JSON.parse(jsonObject);
+    }
+    catch(err)
+    {
+        alert(err);
+    }
+  
+  if(confirm("Are you sure you want to delete this contact?"))
+  {
+    // var i = r.parentNode.parentNode.rowIndex;
+    
+    // var del_FirstName = document.getElementById("First Name").value;
+    
+    var table = document.getElementById("contactsData");
+    var tLength = table.tBodies.length;
+    var tr = document.getElementById("contactsData").rows[tLength].cells.length;
+    
+    var j = "Table height " + (tLength+1) + "\nTable length "  + tr;
+
+
+    // var i = getElementByClass("btn btn-primary").id;
+    var i = getButtonIndex()
+
+    var k = "Button number " + i;
+    /*
+    var del_FirstName = jsonObject[i]["FirstName"];
+    var del_LastName = jsonObject[i]["LastName"];
+    var del_PhoneNumber = jsonObject[i]["PhoneNumber"];
+    var del_Email = jsonObject[i]["Email"];
+    var s = "Yeet " + del_FirstName + " outta here!";
+    */
+    alert(i);
+   
+    
+    // document.getElementById("contactsData").deleteRow(0);
+
+    // var toBeDeleted ="";
+    // var jsonPayload = '{ "UserID" : "' + window.name + '", "FirstName" : "' + del_FirstName + '", "LastName" : "' + del_LastName + '", "PhoneNumber" : "' + del_PhoneNumber + '", "Email" : "' + del_Email + '" }';
+    /*
+    var url = urlBase + '/DeleteContact';
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, false);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try
+    {
+
+      xhr.send(jsonPayload); // sends to server, used for POST requests
+
+      var jsonObject = JSON.parse(xhr.responseText);
+      jsonObject = JSON.parse(jsonObject);
+
+    }
+    catch(err)
+    {
+      // document.getElementById("loginResult").innerHTML = err.message;
+      alert(err);
+    }
+    */
+    // delete from DB
+    // remove from table
+    // force reload 
+    getContacts();
+  }
+  // alert("delete clicked!");
+  
+  
 }
 
 function searchContacts() {
